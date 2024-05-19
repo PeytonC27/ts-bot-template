d99bd6ef5970d1406f7b850be8f3fa3c95361a63
Peyton Crua <peytoncrua27@gmail.com>
2024-05-19 01:42:16 -0600

    Cleaned up dist folder, added template .env file

--- dist/Utilities.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-async function reply(interaction, message) {
-    interaction.reply({ content: message, ephemeral: true, fetchReply: true }).then(msg => setTimeout(() => msg.delete(), 5000));
-}
-module.exports = { reply };
